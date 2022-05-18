50a02a4ff19facb99f03f70bd175ff5ab00c8cb0
adamfouche <adamfouche2002@gmail.com>
2022-05-18 14:54:47 +0700

    Design Trello front-end.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,97 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="trello-adam-master">
+      <nav className="navbar app">Navbar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>todo list</header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.dribbble.com/userupload/2614664/file/original-707d1a26b873877c3d6af761affec459.png?filters:format(webp)?filters%3Aformat%28webp%29=&compress=1&resize=1000x750"
+                alt="Fly-img"
+              />
+              Title: HUSTLE
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>todo list</header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.dribbble.com/userupload/2614664/file/original-707d1a26b873877c3d6af761affec459.png?filters:format(webp)?filters%3Aformat%28webp%29=&compress=1&resize=1000x750"
+                alt="Fly-img"
+              />
+              Title: HUSTLE
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>todo list</header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.dribbble.com/userupload/2614664/file/original-707d1a26b873877c3d6af761affec459.png?filters:format(webp)?filters%3Aformat%28webp%29=&compress=1&resize=1000x750"
+                alt="Fly-img"
+              />
+              Title: HUSTLE
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>todo list</header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.dribbble.com/userupload/2614664/file/original-707d1a26b873877c3d6af761affec459.png?filters:format(webp)?filters%3Aformat%28webp%29=&compress=1&resize=1000x750"
+                alt="Fly-img"
+              />
+              Title: HUSTLE
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>todo list</header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.dribbble.com/userupload/2614664/file/original-707d1a26b873877c3d6af761affec459.png?filters:format(webp)?filters%3Aformat%28webp%29=&compress=1&resize=1000x750"
+                alt="Fly-img"
+              />
+              Title: HUSTLE
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
