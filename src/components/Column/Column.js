ea3d99a44004ba59504c37414d5215c0b0430366
adamfouche <adamfouche2002@gmail.com>
2022-05-20 07:46:45 +0700

    initialize sample data

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,24 +1,19 @@
 import React from "react";
 import "./Column.scss";
-import Task from "components/Task/Task";
-const Column = () => {
+import Card from "components/Card/Card";
+import { mapOrder } from "utilities/sorts";
+const Column = (props) => {
+  const { column } = props;
+  // console.log("COLUMNS", column);
+  const cards = mapOrder(column.cards, column.cardOrder, "id");
+
   return (
     <div className="column">
-      <header>todo list</header>
-      <ul className="task-list">
-        <Task />
-        <li className="task-item">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </li>
-        <li className="task-item">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </li>
-        <li className="task-item">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </li>
-        <li className="task-item">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </li>
+      <header>{column.title}</header>
+      <ul className="card-list">
+        {cards.map((card, index) => (
+          <Card key={index} card={card} />
+        ))}
       </ul>
       <footer>Add another card</footer>
     </div>
