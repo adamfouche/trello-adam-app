8c4e56ebf808be26b624c39c48d93bb9d154962d
adamfouche <adamfouche2002@gmail.com>
2022-05-23 08:50:11 +0700

    handle state change when dnd.

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Draggable } from 'react-smooth-dnd';
+import React, { useEffect, useState } from 'react'
+import { Container, Draggable } from 'react-smooth-dnd'
 
-import { isEmpty } from 'lodash';
+import { isEmpty } from 'lodash'
 
-import './BoardContent.scss';
+import './BoardContent.scss'
 
-import Column from 'components/Column/Column';
-import { mapOrder } from 'utilities/sorts';
+import Column from 'components/Column/Column'
+import { mapOrder } from 'utilities/sorts'
+import { applyDrag } from 'utilities/dragDrop'
 
-import { initialData } from 'actions/initialData';
+import { initialData } from 'actions/initialData'
 
 const BoardContent = () => {
-  const [board, setBoard] = useState({});
-  const [columns, setColumns] = useState([]);
+  const [board, setBoard] = useState({})
+  const [columns, setColumns] = useState([])
 
   useEffect(() => {
     const boardFromDB = initialData.boards.find(
       (board) => board.id === 'board-1'
-    );
+    )
     if (boardFromDB) {
-      setBoard(boardFromDB);
+      setBoard(boardFromDB)
 
       // sort columns
 
@@ -30,16 +31,33 @@ const BoardContent = () => {
       //   );
       // });
       // setColumns(boardFromDB.columns);
-      setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'));
+      setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
     }
-  }, []);
+  }, [])
 
   if (isEmpty(board)) {
-    return <div className='notfound'>Board not found</div>;
+    return <div className='notfound'>Board not found</div>
   }
   const onColumnDrop = (dropResult) => {
-    // console.log(dropResult);
-  };
+    let newColumns = [...columns]
+    newColumns = applyDrag(newColumns, dropResult)
+
+    let newBoard = { ...board }
+    newBoard.columnOrder = newColumns.map((col) => col.id)
+    newBoard.columns = newColumns
+
+    setColumns(newColumns)
+    setBoard(newBoard)
+  }
+  const onCardDrop = (columnId, dropResult) => {
+    if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
+      let newColumns = [...columns]
+      let currentColumn = newColumns.find((col) => col.id === columnId)
+      currentColumn.cards = applyDrag(currentColumn.cards, dropResult)
+      currentColumn.cardOrder = currentColumn.cards.map((card) => card.id)
+      setColumns(newColumns)
+    }
+  }
   return (
     <div className='board-content'>
       <Container
@@ -56,13 +74,17 @@ const BoardContent = () => {
         {columns.map((column, idx) => {
           return (
             <Draggable key={idx}>
-              <Column column={column} />
+              <Column column={column} onCardDrop={onCardDrop} />
             </Draggable>
-          );
+          )
         })}
       </Container>
+      {/* Add new Column */}
+      <div className='add-new-column'>
+        <i className='fa fa-plus' /> Add another column
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default BoardContent;
+export default BoardContent
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { Container, Draggable } from 'react-smooth-dnd';
+import React from 'react'
+import { Container, Draggable } from 'react-smooth-dnd'
 
-import './Column.scss';
-import Card from 'components/Card/Card';
-import { mapOrder } from 'utilities/sorts';
+import './Column.scss'
+import Card from 'components/Card/Card'
+import { mapOrder } from 'utilities/sorts'
 const Column = (props) => {
-  const { column } = props;
-  // console.log("COLUMNS", column);
-  const cards = mapOrder(column.cards, column.cardOrder, 'id');
-  const onCardDrop = (dropResult) => {
-    // console.log(dropResult);
-  };
+  const { column, onCardDrop } = props
+  // console.log('COLUMNS', column)
+  const cards = mapOrder(column.cards, column.cardOrder, 'id')
+
   return (
     <div className='column'>
       <header className='column-drag-handle'>{column.title}</header>
       <div className='card-list'>
         <Container
           groupName='adam-columns'
-          onDrop={onCardDrop}
+          onDrop={(dropResult) => onCardDrop(column.id, dropResult)}
           getChildPayload={(index) => cards[index]}
           dragClass='card-ghost'
           dropClass='card-ghost-drop'
@@ -35,9 +33,14 @@ const Column = (props) => {
           ))}
         </Container>
       </div>
-      <footer>Add another card</footer>
+      <footer>
+        <div className='footer-actions'>
+          <i className='fa fa-plus icon'></i>
+          Add another card
+        </div>
+      </footer>
     </div>
-  );
-};
+  )
+}
 
-export default Column;
+export default Column
